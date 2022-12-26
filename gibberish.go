16e76626cfff49523251ff46fc5fc2a8eaa150a2
Corey Prophitt <git@prophitt.me>
2022-12-26 05:12:47 -0800

    Added runeset to classifier

--- gibberish.go
+++ gibberish.go
@@ -6,7 +6,7 @@ import "io"
 
 classifier := gibberish.New()
 
-classifier.Train("some big text")
+classifier.Train(strings.NewReader("some big text"))
 
 if ok, err := classifier.Check("something"); err == nil && ok {
 	// Gibberish
@@ -14,7 +14,12 @@ if ok, err := classifier.Check("something"); err == nil && ok {
 
 */
 
+const (
+	DefaultCharset = "abcdefghijklmnopqrstuvwxyz "
+)
+
 type Classifier struct {
+	runes map[rune]struct{}
 }
 
 func (c *Classifier) Train(r io.Reader) error {
@@ -25,7 +30,19 @@ func (c *Classifier) Check(junk string) (bool, error) {
 	return false, nil
 }
 
-// New creates a nrew classifier that is ready for use.
-func New() *Classifier {
-	return &Classifier{}
+// New creates a new classifier that is ready for use.
+func New(runsets ...string) *Classifier {
+	if len(runsets) == 0 {
+		runsets = append(runsets, DefaultCharset)
+	}
+
+	classifier := &Classifier{runes: map[rune]struct{}{}}
+
+	for _, runes := range runsets {
+		for _, char := range runes {
+			classifier.runes[char] = struct{}{}
+		}
+	}
+
+	return classifier
 }
