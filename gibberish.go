85107f9895bed6d522a6a342e1ef7bc9054bab0f
Corey Prophitt <git@prophitt.me>
2022-12-26 04:55:50 -0800

    Stubbed our main methods

--- gibberish.go
+++ gibberish.go
@@ -1,1 +1,31 @@
 package gibberish
+
+import "io"
+
+/*
+
+classifier := gibberish.New()
+
+classifier.Train("some big text")
+
+if ok, err := classifier.Check("something"); err == nil && ok {
+	// Gibberish
+}
+
+*/
+
+type Classifier struct {
+}
+
+func (c *Classifier) Train(r io.Reader) error {
+	return nil
+}
+
+func (c *Classifier) Check(junk string) (bool, error) {
+	return false, nil
+}
+
+// New creates a nrew classifier that is ready for use.
+func New() *Classifier {
+	return &Classifier{}
+}
